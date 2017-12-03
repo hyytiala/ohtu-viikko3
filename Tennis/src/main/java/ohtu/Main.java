a9bdbcbe29c331f4bac41000de245f49f4d56f53
hyytiala <otto.hyytiala@gmail.com>
2017-12-03 14:39:56 +0200

    master 10

--- Tennis/src/main/java/ohtu/Main.java
+++ Tennis/src/main/java/ohtu/Main.java
@@ -11,24 +11,25 @@ package ohtu;
  */
 public class Main {
     public static void main(String[] args) {
-    TennisGame game = new TennisGame("player1", "player2");
-    System.out.println("Tervetuloa tannikseen");
+        TennisGame game = new TennisGame("player1", "player2");
+        System.out.println("Tervetuloa tannikseen");
 
-    System.out.println(game.getScore());
+        System.out.println(game.getScore());
 
-    game.wonPoint("player1");
-    System.out.println(game.getScore());
+        game.wonPoint("player1");
+        System.out.println(game.getScore());
 
-    game.wonPoint("player1");
-    System.out.println(game.getScore());
+        game.wonPoint("player1");
+        System.out.println(game.getScore());
 
-    game.wonPoint("player2");
-    System.out.println(game.getScore());
+        game.wonPoint("player2");
+        System.out.println(game.getScore());
 
-    game.wonPoint("player1");
-    System.out.println(game.getScore());
+        game.wonPoint("player1");
+        System.out.println(game.getScore());
 
-    game.wonPoint("player1");
-    System.out.println(game.getScore());
+        game.wonPoint("player1");
+        System.out.println(game.getScore());
+        System.out.println("Kiitos ja heihei.");
 }
 }
